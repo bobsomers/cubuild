15ddc672f362bcf09b1a3f101c10564723f626e0
Bob Somers <bob@bobsomers.com>
2011-03-29 01:43:12 -0700

    Removed support for the .c++ file extension and added a function that returnes the relative file path from the absolute one based on the current directory.

--- src/files.lua
+++ src/files.lua
@@ -21,8 +21,7 @@ function get_source_files(path)
                    last3 == ".cc" or
                    last3 == ".cu" or
                    last4 == ".cpp" or
-                   last4 == ".cxx" or
-                   last4 == ".c++" then
+                   last4 == ".cxx" then
                     table.insert(found, f)
                 end
             end
@@ -35,4 +34,8 @@ end
 function get_object_file(src_file)
     local dot = string.find(src_file, ".", -4, true)
     return string.sub(src_file, 1, dot) .. "o"
+end
+
+function get_relative_path(file)
+    return string.sub(file, string.len(lfs.currentdir()) + 2)
 end
